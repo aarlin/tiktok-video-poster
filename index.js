7dfea69fbe814ffe47862dcf11c2ac7d696e980a
Aaron Lin <aaronlin256@gmail.com>
2021-02-23 01:51:10 -0500

    comment out work to split video until finished

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Client();
 
 require('dotenv').config();
 const splitFile = require('split-file');
+const ffmetadata = require('ffmetadata');
 const fileUtils = require('./fileUtils');
 const config = require('./config').botConfiguration;
 const utils = require('./utils');
@@ -26,6 +27,13 @@ client.on('message', async (msg) => {
 			const fileName = fileUtils.getFileName(tiktokVideo);
 			const fileSize = await fileUtils.getFileSize(fileName);
 
+			console.log(fileName, fileSize);
+
+			// ffmetadata.read(fileName, function(err, data) {
+			// 	if (err) console.error("Error reading metadata", err);
+			// 	else console.log(data);
+			// });
+
 			if (fileName && fileSize <= config.MAX_FILE_SIZE_UPLOAD) {
 				console.log(fileName);
 				const attachment = new MessageAttachment(fileName);
@@ -33,20 +41,21 @@ client.on('message', async (msg) => {
 				await fileUtils.deleteFile(fileName);
 			}
 			else {
-				console.log(fileSize);
-				console.log(fileSize / 2.0);
-				// Remove file format from video and split into pieces
-				const fileWithoutFormat = await fileUtils.changeFileName(fileName, fileUtils.getNameWithoutFileFormat(fileName));
-				const splitFileNames = await splitFile.splitFileBySize(fileWithoutFormat, fileSize / 2.0);
+				await msg.reply(`Could not locate video or file size exceeded ${utils.convertBytesToMB(config.MAX_FILE_SIZE_UPLOAD)} MB.`);
+				// console.log(fileSize);
+				// console.log(fileSize / 2.0);
+				// // Remove file format from video and split into pieces
+				// const fileWithoutFormat = await fileUtils.changeFileName(fileName, fileUtils.getNameWithoutFileFormat(fileName));
+				// const splitFileNames = await splitFile.splitFileBySize(fileWithoutFormat, fileSize / 2.0);
 
-				// Add back file format to each split video
-				for (const [index, file] of splitFileNames.entries()) {
-					const newFileName = await fileUtils.changeFileName(file, fileUtils.appendFileFormat(file, config.DOWNLOADED_FILE_FORMAT));
-					console.log(index, file, newFileName);
-					const attachment = new MessageAttachment(newFileName);
-					await msg.channel.send(utils.createSplitVideoText(selectedMetadata, index, splitFileNames.length), attachment);
-					// await fileUtils.deleteFile(newFileName);
-				}
+				// // Add back file format to each split video
+				// for (const [index, file] of splitFileNames.entries()) {
+				// 	const newFileName = await fileUtils.changeFileName(file, fileUtils.appendFileFormat(file, config.DOWNLOADED_FILE_FORMAT));
+				// 	console.log(index, file, newFileName);
+				// 	const attachment = new MessageAttachment(newFileName);
+				// 	await msg.channel.send(utils.createSplitVideoText(selectedMetadata, index, splitFileNames.length), attachment);
+				// 	// await fileUtils.deleteFile(newFileName);
+				// }
 			}
 		}
 		catch (error) {
